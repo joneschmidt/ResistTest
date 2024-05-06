d61f0250da1c11f4c73482011d4373eb3868e2a6
Jon Schmidt <39682093+joneschmidt@users.noreply.github.com>
2024-05-06 15:43:35 -0700

    Add files via upload
    Redesigned ResistTest to assist in setting the Sentivity value in GradeCrossing and other sketches.

--- ResistTest.ino
+++ ResistTest.ino
@@ -3,28 +3,28 @@
 // MSLoop is the length of the integration
 // DlyInLoop is time between sampling within the integration loop
 // DlyOutLoop is the cycle time between sample sets
+// 2024-05-06 JES redesign to report new data technique
 // 22/02/21 JES report % etc
 // 21/12/10 JES allow 0 as sensor
 // 2/21/21
 #define NumS  4
 //const int Snsrs[NumS] = {A0, A1, A2, A3, A4, A5, A6, A7};// analog sensors
-const int Snsrs[NumS] = {A0, A1, A2, A3};// analog sensors
+const int Snsrs[NumS] = {A0, A1, A2, 0};// analog sensors
 /*
 #define NumS  8
 const int Snsrs[NumS] = {A0,A1,A2,A3, A4,A5,A6,A7};// analog sensors
 */
 #define Mode  INPUT
 const int MSLoop = 33;
-const int DlyInLoop = 1;
+const int DlyInLoop = 0;
 const int DlyOutLoop = 2000;
+const int RecalibrateMts = 1;
 
 int idx;
 long int TimeStrt;
-long int Smpl, Rnge, Prcnt;
-long int Smpls;
+long int Prcnt, Smpls;
 long int Vals[NumS];
 long int Highest[NumS];
-long int Lowest[NumS];
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,26 +33,21 @@ void setup() {
   } // idx
     for (idx = 0; idx < NumS;++idx){
       Vals[idx] = Highest[idx] = 0;
-      Lowest[idx] = 9999;
     }//for
 
   Serial.begin(9600);
   Serial.println("Startup");
+  TimeStrt = millis();
   } // setup
 
 void loop() {
   // put your main code here, to run repeatedly:
-  TimeStrt = millis();
   Smpls = 0;
     for (idx = 0; idx < NumS;++idx){Vals[idx] = 0;}
   do {
     for (idx = 0; idx < NumS;++idx){
       if (Snsrs[idx] > 0){
-      Smpl = analogRead(Snsrs[idx]);
-//  Serial.println(Smpl);
-      Vals[idx] += Smpl;
-      if (Smpl > Highest[idx]){Highest[idx] = Smpl;}
-      if (Smpl < Lowest [idx]){Lowest[idx]  = Smpl;}
+      Vals[idx] += analogRead(Snsrs[idx]);
     }}
     ++Smpls;
     delay(DlyInLoop);
@@ -66,26 +61,23 @@ Serial.println(millis());
 
 for (idx = 0; idx < NumS;++idx){
   if (Snsrs[idx] > 0){
-  Serial.print(idx);
-  Serial.print(" Pin: ");
-  Serial.print(Snsrs[idx]);
-  Serial.print(" CurVal: ");
-  Prcnt = (Vals[idx] / Smpls);
-  Serial.print(Prcnt);
-  Serial.print(" FmHi: ");
-  Prcnt = Highest[idx] - Prcnt;
-  Serial.print(Prcnt);
-  Serial.print("/");
-  Rnge = Highest[idx] - Lowest[idx];
-  Prcnt = (Prcnt) * 100 / Rnge;
-  Serial.print(Rnge);
-  Serial.print(" ");
-  Serial.print(Prcnt);
-  Serial.print("% Hi: ");
-  Serial.print(Highest[idx]);
-  Serial.print(" Lo: ");
-  Serial.println(Lowest[idx]);
+  Vals[idx] /= Smpls;
+  if (Highest[idx] < Vals[idx]){Highest[idx] = Vals[idx];}
+  Serial.print(" Pin: " + String(Snsrs[idx]));
+  Serial.print(" High: " + String(Highest[idx]));
+  Prcnt = (Vals[idx]);
+  Serial.print(" CurVal: " + String(Prcnt));
+  Prcnt = 100.-(Prcnt * 100.) / Highest[idx];
+  Serial.print(" % drop: ");
+  Serial.println(String(Prcnt));
 }} // for
  
+ // Serial.println("Recalibrate at " + String((TimeStrt + RecalibrateMts*60000.) - millis()));
+ if ((TimeStrt + RecalibrateMts*60000.) < millis()){
+   for (idx = 0; idx < NumS;++idx){
+      Highest[idx] = 0;}//for
+  Serial.println("Recalibrate");
+ };
+
   delay(DlyOutLoop);
 }
