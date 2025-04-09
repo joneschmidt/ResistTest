a1f033cc4840074a9da05cf9081d9e6e24a8f915
Jon Schmidt <39682093+joneschmidt@users.noreply.github.com>
2025-04-09 14:14:46 -0700

    Add files via upload

--- ResistTest.ino
+++ ResistTest.ino
@@ -3,25 +3,37 @@
 // MSLoop is the length of the integration
 // DlyInLoop is time between sampling within the integration loop
 // DlyOutLoop is the cycle time between sample sets
-// 2024-05-06 JES redesign to report new data technique
-// 22/02/21 JES report % etc
-// 21/12/10 JES allow 0 as sensor
-// 2/21/21
-#define NumS  4
+// 2024-07-08 JES fix for 0
+// 2024-05-09 JES redesign to report new data technique
+// *************************************************************************
+// This code is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed WITHOUT ANY WARRANTY; 
+// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// http://www.gnu.org/licenses/.
+// *************************************************************************
+/* #define NumS  4
 //const int Snsrs[NumS] = {A0, A1, A2, A3, A4, A5, A6, A7};// analog sensors
-const int Snsrs[NumS] = {A0, A1, A2, 0};// analog sensors
-/*
+const int Snsrs[NumS] = {A0, A1, A2, A3};// analog sensors
+*/
 #define NumS  8
 const int Snsrs[NumS] = {A0,A1,A2,A3, A4,A5,A6,A7};// analog sensors
-*/
+//
 #define Mode  INPUT
 const int MSLoop = 33;
 const int DlyInLoop = 0;
-const int DlyOutLoop = 2000;
+const int DlyOutLoop = 5000;
 const int RecalibrateMts = 1;
 
 int idx;
-long int TimeStrt;
+long int TimeStrt, RecalStrt;
 long int Prcnt, Smpls;
 long int Vals[NumS];
 long int Highest[NumS];
@@ -29,28 +41,28 @@ long int Highest[NumS];
 void setup() {
   // put your setup code here, to run once:
   for (idx = 0; idx < NumS;++idx){
-  if (Snsrs[idx] > 0) {pinMode(Snsrs[idx], Mode);}
+  if (Snsrs[idx] > 0) {pinMode(Snsrs[idx], INPUT);}
   } // idx
     for (idx = 0; idx < NumS;++idx){
       Vals[idx] = Highest[idx] = 0;
     }//for
-
+  RecalStrt = millis();
   Serial.begin(9600);
   Serial.println("Startup");
-  TimeStrt = millis();
   } // setup
 
 void loop() {
   // put your main code here, to run repeatedly:
   Smpls = 0;
-    for (idx = 0; idx < NumS;++idx){Vals[idx] = 0;}
+  TimeStrt = millis();
+  for (idx = 0; idx < NumS;++idx){Vals[idx] = 0;}
   do {
     for (idx = 0; idx < NumS;++idx){
       if (Snsrs[idx] > 0){
       Vals[idx] += analogRead(Snsrs[idx]);
     }}
     ++Smpls;
-    delay(DlyInLoop);
+    if (DlyInLoop > 0) {delay(DlyInLoop);}
   } while (TimeStrt + MSLoop > millis());
 
 Serial.print(Smpls);
@@ -63,20 +75,21 @@ for (idx = 0; idx < NumS;++idx){
   if (Snsrs[idx] > 0){
   Vals[idx] /= Smpls;
   if (Highest[idx] < Vals[idx]){Highest[idx] = Vals[idx];}
-  Serial.print(" Pin: " + String(Snsrs[idx]));
+  Serial.print(" Pin: " + String(idx) + " " + String(Snsrs[idx]));
   Serial.print(" High: " + String(Highest[idx]));
   Prcnt = (Vals[idx]);
   Serial.print(" CurVal: " + String(Prcnt));
-  Prcnt = 100.-(Prcnt * 100.) / Highest[idx];
+  Prcnt = (Highest[idx] > 0)?100.-(Prcnt * 100.) / Highest[idx]:0;
   Serial.print(" % drop: ");
   Serial.println(String(Prcnt));
 }} // for
  
  // Serial.println("Recalibrate at " + String((TimeStrt + RecalibrateMts*60000.) - millis()));
- if ((TimeStrt + RecalibrateMts*60000.) < millis()){
+ if ((RecalStrt + RecalibrateMts*60000.) < millis()){
    for (idx = 0; idx < NumS;++idx){
       Highest[idx] = 0;}//for
   Serial.println("Recalibrate");
+  RecalStrt = millis();
  };
 
   delay(DlyOutLoop);
